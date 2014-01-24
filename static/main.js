589e1ee45922b652389c522e65245f5e39f4f703
Jacob Struzik <jacob@onemightyroar.com>
2014-01-24 17:48:10 -0500

    forgot to add js file

--- static/main.js
+++ static/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	section_id = 1;
 	resource_id = 1;
+	header_id = 1;
+	code_id = 1;
+	reponse_id = 1;
 	$('.section_button').on('click', function(){
 		section_id++;
 		var section_clone = $('#section_0').clone(true).attr('id', 'section_' + section_id);
@@ -17,6 +20,25 @@ $(document).ready(function() {
 		var id = $(this).attr('id').split('_')[2];
 		var resource_clone = $('#resource_0_0').clone(true).attr('id', 'resource_' + id + '_' + resource_id);
 		resource_clone.appendTo('#resources_' + id).show();
+	});
+	$('.res_header_button').on('click', function(){
+		header_id++;
+		var id = $(this).attr('id').split('_')[3];
+		var header_clone = $('#header_0_0_0').clone(true).attr('id', 'header_' + id + '_' + resource_id + '_' + header_id );
+		console.log(header_clone);
+		header_clone.appendTo('#headers_' + id).show();
+	});
+	$('.res_code_button').on('click', function(){
+		code_id++;
+		var id = $(this).attr('id').split('_')[3];
+		var code_clone = $('#code_0_0_0').clone(true).attr('id', 'code_' + id + '_' + resource_id + '_' + code_id );
+		code_clone.appendTo('#codes_' + id).show();
+	});
+	$('.res_resp_button').on('click', function(){
+		reponse_id++;
+		var id = $(this).attr('id').split('_')[3];
+		var response_clone = $('#response_0_0_0').clone(true).attr('id', 'response_' + id + '_' + resource_id + '_' + reponse_id );
+		response_clone.appendTo('#response_' + id).show();
 	});
 	$('.section_resource_button').on('click', function(){
 		var id = $(this).attr('id').split('_')[3];
